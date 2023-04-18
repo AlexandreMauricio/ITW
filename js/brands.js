461290c2203ff0e964c4e5e35d9fb7902128aebe
Alexandre Mauricio <alexandre.mauricio@ipcbcampus.pt>
2023-04-18 16:37:20 +0100

    Não foi tudo substituido, só umas alterações

---
+++ js/brands.js
@@ -0,0 +1,68 @@
+var brands = [{
+        brand: "Audi",
+        slogan: "Vorsprung durch Technik",
+        logo: "audi.png"
+    }, {
+        brand: "Toyota",
+        slogan: "Let's go places",
+        logo: "toyota.png"
+    }, {
+        brand: "Ford",
+        slogan: "Go further",
+        logo: "ford.png"
+    }, {
+        brand: "Chevrolet",
+        slogan: "Find new roads",
+        logo: "chevrolet.png"
+    }, {
+        brand: "Honda",
+        slogan: "The power of Dreams",
+        logo: "honda.jpeg"
+    }, {
+        brand: "Nissan",
+        slogan: "Innovation that excites",
+        logo: "nissan.png"
+    }, {
+        brand: "Jeep",
+        slogan: "There's only one",
+        logo: "jeep.jpg"
+    }, {
+        brand: "Hyundai",
+        slogan: "New thinking. New possibilities",
+        logo: "hyundai.jpg"
+    }, {
+        brand: "Kia",
+        slogan: "Movement that inspires",
+        logo: "kia.jpg"
+    }, {
+        brand: "Mercedes-Benz",
+        slogan: "The Best or Nothing",
+        logo: "mercedes.jpg"
+    }, {
+        brand: "BMW",
+        slogan: "The Ultimate Driving Machine",
+        logo: "bmw.png"
+    }, {
+        brand: "Volkswagen",
+        slogan: "Das Auto",
+        logo: "volkswagen.jpg"
+    }, {
+        brand: "Subaru",
+        slogan: "Confidence in Motion",
+        logo: "subaru.png"
+    },
+    {
+        brand: "Lexus",
+        slogan: "Experience Amazing",
+        logo: "lexus.png"
+    },
+    {
+        brand: "Porsche",
+        slogan: "There is no substitute",
+        logo: "porsche.png"
+    },{
+        brand: "Volvo",
+        slogan: "For Life",
+        logo: "volvo.jpg"
+    }
+]
